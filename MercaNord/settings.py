0948a12e31b32c582e161ea6f19e204e049c2445
SebastianMaldonado <98917583+SebastianMaldonado@users.noreply.github.com>
2024-05-31 00:10:16 -0500

    Update settings.py

--- MercaNord/settings.py
+++ MercaNord/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 MEDIA_URL = 'static/'
-STATIC_URL = '/staticfiles/'
+STATIC_URL = ''
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
